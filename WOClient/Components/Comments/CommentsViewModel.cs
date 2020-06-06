256fe3a0a82111124e1ff9e03ef59a6a344aae31
Amir Liberzon <amir.lib@gmail.com>
2020-06-06 17:14:19 +0300

    added comments view

--- WOClient/Components/Comments/CommentsViewModel.cs
+++ WOClient/Components/Comments/CommentsViewModel.cs
@@ -1,8 +1,37 @@
-﻿using WOClient.Components.Base;
+﻿using System.Collections.ObjectModel;
+using WOClient.Components.Base;
+using WOClient.Models;
 
 namespace WOClient.Components.Comments
 {
     public class CommentsViewModel: BaseViewModel, ICommentsViewModel
     {
+        public CommentsViewModel()
+        {
+            Comments = new ObservableCollection<Comment>();
+
+            var task1 = new Comment
+            {
+                TaskName = "Comment 1",
+                EmployeeName = "Amir L",
+                EmployeeComment = "I have a comment",
+                Reply = "Very good comment. Task is very good."
+            };
+
+            var task2 = new Comment
+            {
+                TaskName = "Comment 2",
+                EmployeeName = "Inbal B",
+                EmployeeComment = "I have another comment",
+                Reply = "Not so Very good comment. comment is very good."
+            };
+
+            Comments.Add(task1);
+            Comments.Add(task2);
+        }
+
+        #region Properties
+        public ObservableCollection<Comment> Comments { get; set; }
+        #endregion
     }
 }
---
+++ WOClient/Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace WOClient.Models
+{
+    public class Comment
+    {
+        public string TaskName { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeComment { get; set; }
+        public string Reply { get; set; }
+    }
+}
