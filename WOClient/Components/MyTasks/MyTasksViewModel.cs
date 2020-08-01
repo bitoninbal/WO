a33fd6a3e37090caa12e629c4d7fd9d79dbe9386
Amir Liberzon <amir.lib@gmail.com>
2020-08-01 16:41:54 +0300

    added style for MyTask's DataTemplate

--- WOClient/Components/MyTasks/MyTasksViewModel.cs
+++ WOClient/Components/MyTasks/MyTasksViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
 using WOClient.Components.Base;
 using WOClient.Library.Models;
+using WOClient.Resources.Commands;
 
 namespace WOClient.Components.MyTasks
 {
@@ -8,11 +11,23 @@ namespace WOClient.Components.MyTasks
     {
         public MyTasksViewModel()
         {
-            Tasks = new ObservableCollection<MyTask>();
+            AddCommentCommand = new RelayCommand(AddComment);
+            Tasks             = new ObservableCollection<MyTask>();
         }
+
+        #region ICommand
+        public ICommand AddCommentCommand { get; }
+        #endregion
 
         #region Properties
         public ObservableCollection<MyTask> Tasks { get; set; }
+        #endregion
+
+        #region Private Methods
+        private void AddComment()
+        {
+            MessageBox.Show("Hello");
+        }
         #endregion
     }
 }
